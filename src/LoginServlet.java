35089920f320c340b085dfd2a9e1dd324cd5c2d2
Chengxi Li <chengl47@uci.edu>
2023-04-28 23:24:43 -0700

    Login API created, not tested

---
+++ src/LoginServlet.java
@@ -0,0 +1,100 @@
+import com.google.gson.JsonObject;
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.sql.*;
+
+/* Login Servlet
+ * Endpoint: POST /api/login
+ * Request:
+ *      username: String
+ *      password: String
+ * Response:
+ *      (status code):
+ *          200 - OK
+ *          401 - Credential Incorrect
+ *          500 - Internal Server Error
+ *      (session):
+ *          name: String
+ */
+@WebServlet(name = "LoginServlet", urlPatterns = "/api/login")
+public class LoginServlet extends HttpServlet {
+    private DataSource dataSource;
+
+    public void init(ServletConfig config) {
+        try {
+            dataSource = (DataSource) new InitialContext().lookup("java:comp/env/jdbc/moviedb");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Check if email (username) exists
+    private boolean checkUsername(Connection conn, String username) throws SQLException {
+
+        String query = "SELECT EXISTS(SELECT * FROM `customers` WHERE `email` = ?)";
+        try (PreparedStatement statement = conn.prepareStatement(query)) {
+            statement.setString(1, username);
+            try (ResultSet rs = statement.executeQuery(query)) {
+                // Get first row and first column
+                rs.first();
+                return rs.getBoolean(1);
+            }
+        }
+    }
+
+    // Check if password is correct given the username (email)
+    private boolean checkPassword(Connection conn, String username, String password) throws SQLException {
+        // Check if email (username) exists
+        String query = "SELECT EXISTS(SELECT * FROM `customers` WHERE `email` = ? AND `password` = ?)";
+        try (PreparedStatement statement = conn.prepareStatement(query)) {
+            statement.setString(1, username);
+            statement.setString(2, password);
+            try (ResultSet rs = statement.executeQuery(query)) {
+                // Get first row and first column
+                rs.first();
+                return rs.getBoolean(1);
+            }
+        }
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        /* This example only allows username/password to be test/test
+        /  in the real project, you should talk to the database to verify username/password
+        */
+        JsonObject responseJsonObject = new JsonObject();
+
+        try (Connection conn = dataSource.getConnection()) {
+            if (checkUsername(conn, username)) {
+                if (checkPassword(conn, username, password)) {
+                    response.setStatus(200);
+                } else {
+                    response.setStatus(401);
+                    responseJsonObject.addProperty("message", "incorrect password");
+                }
+            } else {
+                response.setStatus(401);
+                responseJsonObject.addProperty("message", "user " + username + " doesn't exist");
+            }
+        } catch (Exception e) {
+            response.setStatus(500);
+            responseJsonObject.addProperty("message", e.getMessage());
+        }
+
+        response.setContentType("application/json");
+        response.getWriter().write(responseJsonObject.toString());
+    }
+}
