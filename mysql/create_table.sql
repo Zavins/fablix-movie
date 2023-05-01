e0cb45c88bafe2489c3a8aea11cd2cf36126b35f
Chengxi Li <chengl47@uci.edu>
2023-05-01 01:38:06 -0700

    Update view in create_table.sql

--- mysql/create_table.sql
+++ mysql/create_table.sql
@@ -120,3 +120,29 @@ FROM ((`moviedb`.`stars` `s`
 WHERE ((`m`.`id` = `sm`.`movieId`)
     AND (`s`.`id` = `sm`.`starId`))
 ORDER BY `s`.`id`;
+
+-- Create movie_genre_list_view
+CREATE VIEW `moviedb`.`movie_genre_list_view` AS
+SELECT gm.`movieId`,
+       GROUP_CONCAT(
+               g.`id`, '|', g.`name`
+               ORDER BY g.`name` ASC
+               SEPARATOR ';'
+           ) AS `genreList`
+FROM `moviedb`.`genres_in_movies` gm
+         JOIN `moviedb`.`genres` g ON gm.`genreId` = g.`id`
+GROUP BY gm.`movieId`;
+
+-- Create movie_star_list_view
+CREATE VIEW `moviedb`.`movie_star_list_view` AS
+SELECT sm.`movieId`,
+       GROUP_CONCAT(
+               s.`id`, '|', s.`name`
+               ORDER BY mp.`moviesPlayed` DESC, s.`name`
+               SEPARATOR ';'
+           ) AS `starList`
+FROM `moviedb`.`stars_in_movies` sm
+         JOIN `moviedb`.`stars` s ON sm.`starId` = s.`id`
+         JOIN (SELECT sm2.`starId`, COUNT(*) AS `moviesPlayed` FROM `stars_in_movies` sm2 GROUP BY sm2.`starId`) mp
+              ON sm.`starId` = mp.`starId`
+GROUP BY sm.`movieId`;
