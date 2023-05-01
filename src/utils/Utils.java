9f1129b8982cc057bcd64cfdeea25e12692620b1
Chengxi Li <chengl47@uci.edu>
2023-04-30 21:57:22 -0700

    Update API

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -3,10 +3,32 @@ package utils;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Utils {
     public static String mapToJson(Object data) {
         // Create a Gson object and convert the Map object to JSON
         Gson gson = new GsonBuilder().create();
         return gson.toJson(data);
+    }
+
+    public static String getMovieTitle(Connection conn, String movieId) throws SQLException {
+        String query =
+                "SELECT m.`title`\n" +
+                        "FROM `moviedb`.`movies` m\n" +
+                        "WHERE m.`id` = ?";
+
+        System.out.println(movieId);
+
+        try (PreparedStatement statement = conn.prepareStatement(query)) {
+            statement.setString(1, movieId);
+            try (ResultSet rs = statement.executeQuery()) {
+                rs.next();
+                return rs.getString("title");
+            }
+        }
     }
 }
